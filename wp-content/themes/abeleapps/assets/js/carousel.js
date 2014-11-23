f9785fa5a6f7ad0e9543e0635b347cd28577420b
Callum <callum@abeleapps.com>
2014-11-23 13:14:28 +0000

    removed unused themes

--- wp-content/themes/abeleapps/assets/js/carousel.js
+++
@@ -1,8 +0,0 @@
-$('#myCarousel').carousel({
-    interval: 2000
-});
-
-// Could be slid or slide (slide happens before animation, slid happens after)
-$('#myCarousel').bind('slid', function() {
-    alert("Slide Event")
-})
