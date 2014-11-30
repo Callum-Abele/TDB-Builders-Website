0613babc59a645740ba1db70234e6a0c2b92e959
Callum <callum@abeleapps.com>
2014-11-30 13:17:18 +0000

    added page-our-service

---
+++ wp-content/themes/tdb_theme/page-our-services.php
@@ -0,0 +1,10 @@
+<?php /* Template Name: Our Services */  ?>
+
+<?php get_header(); ?>
+	
+		<div class="band footer">	
+			
+				<?php echo do_shortcode("[huge_it_portfolio id='1']"); ?>				
+		</div><!--end band-->
+
+<?php get_footer(); ?>
