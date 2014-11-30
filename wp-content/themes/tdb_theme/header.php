bafd996b409940a7c4ec77337b11af06f1532a35
Callum <callum@abeleapps.com>
2014-11-30 14:14:36 +0000

    made stuff look better

--- wp-content/themes/tdb_theme/header.php
+++ wp-content/themes/tdb_theme/header.php
@@ -69,20 +69,4 @@
 			</nav><!--end container-->
 
 		</div><!--end band-->	
-		<div class="band header">
-
-			<header class="container main">
-					
-					<div class="sixteen columns">
-						
-					<?php efs_slider(); ?>			
-					
-					</div>
-
-
-			</header><!--end container-->
-
-		</div><!--end band-->
-
-		<div class"band footer"></div>	
 
--- wp-content/themes/tdb_theme/index.php
+++ wp-content/themes/tdb_theme/index.php
@@ -1,4 +1,21 @@
 <?php get_header(); ?>
+
+		<div class="band header">
+
+			<header class="container main">
+					
+					<div class="sixteen columns">
+						
+					<?php efs_slider(); ?>			
+					
+					</div>
+
+
+			</header><!--end container-->
+
+		</div><!--end band-->
+
+		<div class"band footer"></div>	
 <?php include 'page-home.php'; ?>
 <?php get_footer(); ?>
 
